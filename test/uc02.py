835ecfb4868c2046755d76cce08b8419a17eb808
phuongbv <buiphuongaptutc@gmail.com>
2025-04-26 22:44:25 +0700

    fix: test uc02

--- test/uc02.py
+++ test/uc02.py
@@ -1,20 +1,24 @@
 import os
 import random
-from dotenv import load_dotenv
+import time
+
 import psycopg
+from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@localhost:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
+
 def random_offset():
     """Tạo một offset nhỏ trong khoảng ±0.0001 độ (~11m)"""
     return random.uniform(-0.0001, 0.0001)
 
+
 def update_students_location_with_jitter():
+    # 1. Lấy danh sách student_id, latitude, longitude từ assignment + bus_stops
     with psycopg.connect(DATABASE_URL) as conn:
         with conn.cursor() as cur:
-            # 1. Lấy danh sách student_id, latitude, longitude từ assignment + bus_stops
             cur.execute("""
                 SELECT assignments.student_id, bus_stops.latitude, bus_stops.longitude
                 FROM assignments
@@ -24,21 +28,26 @@ def update_students_location_with_jitter():
 
             print(f"Found {len(students)} students to update.")
 
-            # 2. Update từng student
-            for student_id, latitude, longitude in students:
-                lat_offset = latitude + random_offset()
-                lon_offset = longitude + random_offset()
+    if not students or len(students) == 0:
+        return
 
+    # 2. Update từng student
+    for student_id, latitude, longitude in students:
+        lat_offset = latitude + random_offset()
+        lon_offset = longitude + random_offset()
+
+        with psycopg.connect(DATABASE_URL) as conn:
+            with conn.cursor() as cur:
                 cur.execute("""
                     UPDATE students
                     SET latitude = %s, longitude = %s
                     WHERE student_id = %s
                 """, (lat_offset, lon_offset, student_id))
+                conn.commit()
                 print(f"Updated student_id {student_id} to lat={lat_offset}, lon={lon_offset}")
 
-        # Commit toàn bộ thay đổi
-        conn.commit()
-        print("All updates committed successfully.")
+        time.sleep(random.uniform(0.1, 1.0))
+
 
 if __name__ == '__main__':
     update_students_location_with_jitter()
